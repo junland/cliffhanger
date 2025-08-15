489d172eff4da85295dac0a6ad47a3cec76bccea
junland <unlandj2012@gmail.com>
2025-08-15 15:23:04 -0500

    fix: update bootstrap script to use absolute path for TARGET_ROOTFS_PATH and improve error messaging

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ CHANNEL_DATE="stable-2025.08-1"
 CONFIG_SITE="toolchains/${TARGET_ARCH}/etc/config.site"
 LC_ALL=POSIX
 TARGET_ARCH="x86_64"
-TARGET_ROOTFS_PATH="rootfs"
+TARGET_ROOTFS_PATH="$PWD/rootfs"
 TARGET_ROOTFS_SOURCES_PATH="${TARGET_ROOTFS_PATH}/tmp/sources"
 TARGET_ROOTFS_WORK_PATH="${TARGET_ROOTFS_PATH}/tmp/work"
 TARGET_TRIPLET="${TARGET_ARCH}-linux-gnu"
@@ -18,8 +18,9 @@ TOOLCHAIN_TARGET_ARCH="${TARGET_ARCH//_/-}"
 TOOLCHAIN_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/${TOOLCHAIN_TARGET_ARCH}/tarballs/${TOOLCHAIN_TARGET_ARCH}--glibc--${CHANNEL_DATE}.tar.xz"
 
 CURL_OPTS="-L -s"
+CFLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
 
-export LC_ALL CONFIG_SITE
+export LC_ALL CONFIG_SITE CFLAGS
 
 # Variables with shorter names
 WORK="${TARGET_ROOTFS_WORK_PATH}"
@@ -36,7 +37,14 @@ msg() {
     echo "==> $*"
 }
 
-echo "Downloading toolchain from ${TOOLCHAIN_URL}..."
+# clean work directory function
+clean_work_dir() {
+    cd "${TARGET_ROOTFS_PATH}"
+    msg "Cleaning up work directory..."
+    rm -rf "${WORK}/*"
+}
+
+msg "Downloading toolchain from ${TOOLCHAIN_URL}..."
 
 # Download the toolchain and extract it to the appropriate directory
 mkdir -p "${TOOLCHAIN_PATH}"
@@ -45,7 +53,7 @@ curl ${CURL_OPTS} "${TOOLCHAIN_URL}" | tar -xJ -C "${TOOLCHAIN_PATH}" --strip-co
 
 # Make sure relocate script is present and executable
 if [ ! -f "${TOOLCHAIN_PATH}/relocate-sdk.sh" ]; then
-    echo "Error: ${TOOLCHAIN_PATH}/relocate-sdk.sh not found!"
+    msg "Error: ${TOOLCHAIN_PATH}/relocate-sdk.sh not found!"
     exit 1
 else
     chmod +x "${TOOLCHAIN_PATH}/relocate-sdk.sh"
@@ -65,7 +73,7 @@ mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}"
 export PATH="${TOOLCHAIN_PATH}/bin:$PATH"
 
 # Download M4
-msg "Downloading M4..."
+msg "Downloading m4..."
 
 mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}/m4-${M4_VER}"
 
@@ -77,14 +85,72 @@ cp -r "${TARGET_ROOTFS_SOURCES_PATH}/m4-${M4_VER}" "${TARGET_ROOTFS_WORK_PATH}/"
 
 cd "${TARGET_ROOTFS_WORK_PATH}/m4-${M4_VER}"
 
-msg "Configuring M4..."
+msg "Configuring m4..."
 
-CFLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2" ./configure --prefix=/usr --host=${TARGET_TRIPLET} --build=$(build-aux/config.guess)
+./configure --prefix=/usr --host=${TARGET_TRIPLET} --build=$(build-aux/config.guess)
 
-msg "Building M4..."
+msg "Building m4..."
 
 make
 
-msg "Installing M4..."
+msg "Installing m4..."
 
 make install DESTDIR="${TARGET_ROOTFS_PATH}"
+
+clean_work_dir
+
+msg "Downloading ncurses..."
+
+mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}/ncurses-${NCURSES_VER}"
+
+curl ${CURL_OPTS} "https://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VER}.tar.gz" | tar -xz -C "${TARGET_ROOTFS_SOURCES_PATH}/ncurses-${NCURSES_VER}" --strip-components=1
+
+msg "Copying sources of ncurses to work directory..."
+
+cp -r "${TARGET_ROOTFS_SOURCES_PATH}/ncurses-${NCURSES_VER}" "${TARGET_ROOTFS_WORK_PATH}/"
+
+cd "${TARGET_ROOTFS_WORK_PATH}/ncurses-${NCURSES_VER}"
+
+msg "Creating tic program in ncurses..."
+
+mkdir -vp build
+
+pushd build
+
+../configure AWK=gawk
+
+make -C include
+
+popd
+
+make -C progs tic
+
+msg "Configuring ncurses..."
+
+./configure \
+    --prefix=/usr \
+    --host=${TARGET_TRIPLET} \
+    --build=$(./config.guess) \
+    --mandir=/usr/share/man \
+    --with-manpage-format=normal \
+    --with-shared \
+    --without-normal \
+    --with-cxx-shared \
+    --without-debug \
+    --without-ada \
+    --disable-stripping \
+    AWK=gawk
+
+msg "Building ncurses..."
+
+make
+
+msg "Installing ncurses..."
+
+make DESTDIR=${TARGET_ROOTFS_PATH} TIC_PATH=${TARGET_ROOTFS_WORK_PATH}/ncurses-${NCURSES_VER}/build/progs/tic install
+
+ln -sv libncursesw.so ${TARGET_ROOTFS_PATH}/usr/lib/libncurses.so
+
+sed -e 's/^#if.*XOPEN.*$/#if 1/' -i ${TARGET_ROOTFS_PATH}/usr/include/curses.h
+
+clean_work_dir
