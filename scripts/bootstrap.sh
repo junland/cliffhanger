73e05c65ad59c12dad723cd96a66ec83f625743e
junland <unlandj2012@gmail.com>
2025-08-15 10:06:59 -0500

    feat: add bootstrap script for toolchain setup and installation

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set +h
+
+umask 022
+
+CHANNEL_DATE="stable-2025.08-1"
+CONFIG_SITE="toolchains/${TARGET_ARCH}/etc/config.site"
+LC_ALL=POSIX
+TARGET_ARCH="x86_64-v2"
+TARGET_TRIPLET="${TARGET_ARCH}-linux-gnu"
+TARGET_ROOTFS_PATH="rootfs"
+TARGET_ROOTFS_WORK_PATH="${TARGET_ROOTFS_PATH}/tmp/work"
+TARGET_ROOTFS_SOURCES_PATH="${TARGET_ROOTFS_PATH}/tmp/sources"
+TOOLCHAIN_PATH="toolchains/${TARGET_ARCH}"
+TOOLCHAIN_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/${TARGET_ARCH}/tarballs/${TARGET_ARCH}--glibc--${CHANNEL_DATE}.tar.xz"
+
+export LC_ALL CONFIG_SITE
+
+# Versions for temporary tools
+M4_VER="1.4.20"
+NCURSES_VER="6.5-20250531"
+BASH_VER="5.3"
+COREUTILS_VER="9.7"
+
+# msg function that will make echo's pretty.
+msg() {
+    echo "==> $*"
+}
+
+echo "Downloading toolchain from ${TOOLCHAIN_URL}..."
+
+# Download the toolchain and extract it to the appropriate directory
+mkdir -p "${TOOLCHAIN_PATH}"
+
+curl -L "${TOOLCHAIN_URL}" | tar -xJ -C "${TOOLCHAIN_PATH}" --strip-components=1
+
+msg "Relocate the toolchain..."
+
+cd "${TOOLCHAIN_PATH}" && ${TOOLCHAIN_PATH}/relocate-sdk.sh && cd ..
+
+# Create necessary directories
+msg "Creating necessary directories..."
+mkdir -vp "${TARGET_ROOTFS_PATH}"
+mkdir -vp "${TARGET_ROOTFS_WORK_PATH}"
+mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}"
+
+# Setup PATH
+export PATH="${TOOLCHAIN_PATH}/bin:$PATH"
+
+# Download M4
+msg "Downloading M4..."
+
+mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}/m4-${M4_VER}"
+
+curl -L "https://ftp.gnu.org/gnu/m4/m4-${M4_VER}.tar.gz" | tar -xz -C "${TARGET_ROOTFS_SOURCES_PATH}/m4-${M4_VER}" --strip-components=1
+
+msg "Copying sources of m4 to work directory..."
+
+cp -r "${TARGET_ROOTFS_SOURCES_PATH}/m4-${M4_VER}" "${TARGET_ROOTFS_WORK_PATH}/"
+
+cd "${TARGET_ROOTFS_WORK_PATH}/m4-${M4_VER}"
+
+msg "Configuring M4..."
+
+./configure --prefix=/usr --host=${TARGET_TRIPLET} --build=$(build-aux/config.guess)
+
+msg "Building M4..."
+
+make
+
+msg "Installing M4..."
+
+make install DESTDIR="${TARGET_ROOTFS_PATH}"
