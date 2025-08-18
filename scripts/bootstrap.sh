e4bb48f5ee8dade4445de847452f53aac60796ff
junland <unlandj2012@gmail.com>
2025-08-18 15:15:34 -0500

    fix: reconfigure diffutils to use the specified version of automake

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -265,6 +265,9 @@ cp -r "${TARGET_ROOTFS_SOURCES_PATH}/diffutils-${DIFFUTILS_VER}" "${TARGET_ROOTF
 
 cd "${TARGET_ROOTFS_WORK_PATH}/diffutils-${DIFFUTILS_VER}"
 
+# Reconfigure to point to our version of automake
+autoreconf -fiv
+
 msg "Configuring diffutils..."
 
 ./configure \
