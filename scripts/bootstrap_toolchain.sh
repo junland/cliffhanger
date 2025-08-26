c320b6802340efa298fa074137ef30d3a7464142
junland <unlandj2012@gmail.com>
2025-08-26 13:18:07 -0500

    fix: create necessary directories for target root filesystem in bootstrap script

--- scripts/bootstrap_toolchain.sh
+++ scripts/bootstrap_toolchain.sh
@@ -58,6 +58,16 @@ mkdir -vp "${TARGET_ROOTFS_WORK_PATH}"
 mkdir -vp "${TARGET_ROOTFS_SOURCES_PATH}"
 mkdir -vp "${TOOLCHAIN_PATH}"
 
+mkdir -pv $TARGET_ROOTFS_PATH/{etc,var} $TARGET_ROOTFS_PATH/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+    ln -sv usr/$i $TARGET_ROOTFS_PATH/$i
+done
+
+case $(uname -m) in
+x86_64) mkdir -pv $TARGET_ROOTFS_PATH/lib64 ;;
+esac
+
 ##
 # binutils Step
 ##
