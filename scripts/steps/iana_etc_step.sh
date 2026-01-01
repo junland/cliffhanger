436efb53d7f6524d49b847895b4c3e6a3d2fb6c0
John Unland <unlandj2012@gmail.com>
2026-01-01 14:23:41 -0600

    Rename function to step_chroot_iana_etc

--- scripts/steps/iana_etc_step.sh
+++ scripts/steps/iana_etc_step.sh
@@ -3,7 +3,7 @@
 
 IANA_ETC_VER="20250807"
 
-step_iana_etc() {
+step_chroot_iana_etc() {
 	msg "Installing IANA network and services data files..."
 
 	extract_file "${TARGET_ROOTFS_SOURCES_PATH}/iana-etc-${IANA_ETC_VER}.tar.gz" "${TARGET_ROOTFS_WORK_PATH}/iana-etc-${IANA_ETC_VER}"
