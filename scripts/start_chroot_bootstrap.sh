e4b89b02df921920d71a7f90b2f6cfd6c1a26ff7
junland <unlandj2012@gmail.com>
2026-02-09 13:37:06 -0600

    fix: remove TERMINFO variable from chroot environment setup

--- scripts/start_chroot_bootstrap.sh
+++ scripts/start_chroot_bootstrap.sh
@@ -129,7 +129,6 @@ if [ "$ENTER_CHROOT_STANDALONE" = "true" ]; then
 		LC_ALL="$LC_ALL" \
 		PATH=/usr/bin:/usr/sbin:/bin:/sbin \
 		PS1='\u:\w\$ ' \
-		TERMINFO="/usr/share/terminfo" \
 		TERM="$TERM" \
 		/bin/bash --login +h
 else
@@ -140,7 +139,6 @@ else
 		LC_ALL="$LC_ALL" \
 		PATH=/usr/bin:/usr/sbin:/bin:/sbin \
 		PS1='\u:\w\$ ' \
-		TERMINFO="/usr/share/terminfo" \
 		TERM="$TERM" \
 		/bin/bash --login +h -c "/tmp/chroot_bootstrap.sh ${STAGE}"
 fi
