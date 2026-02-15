42b4d4f5b320a0c48d198ea323914e50c9a25fe4
junland <unlandj2012@gmail.com>
2026-02-15 12:19:05 -0600

    Fix formatting in ncurses and util-linux step scripts for consistency

--- scripts/steps/ncurses_step.sh
+++ scripts/steps/ncurses_step.sh
@@ -88,9 +88,9 @@ step_chroot_ncurses() {
 
 	cp -av dest/* /
 
-	for lib in ncurses form panel menu ; do
+	for lib in ncurses form panel menu; do
 		ln -sfv lib${lib}w.so /usr/lib/lib${lib}.so
-		ln -sfv ${lib}w.pc    /usr/lib/pkgconfig/${lib}.pc
+		ln -sfv ${lib}w.pc /usr/lib/pkgconfig/${lib}.pc
 	done
 
 	ln -sfv libncursesw.so /usr/lib/libcurses.so
--- scripts/steps/util_linux_step.sh
+++ scripts/steps/util_linux_step.sh
@@ -43,23 +43,23 @@ step_chroot_util_linux_stage3() {
 
 	msg "Configuring util-linux..."
 
-    ./configure \
-	        --bindir=/usr/bin     \
-            --libdir=/usr/lib     \
-            --runstatedir=/run    \
-            --sbindir=/usr/sbin   \
-            --disable-chfn-chsh   \
-            --disable-login       \
-            --disable-nologin     \
-            --disable-su          \
-            --disable-setpriv     \
-            --disable-runuser     \
-            --disable-pylibmount  \
-            --disable-liblastlog2 \
-            --disable-static      \
-            --without-python      \
-            ADJTIME_PATH=/var/lib/hwclock/adjtime \
-            --docdir=/usr/share/doc/util-linux-${UTIL_LINUX_VER}
+	./configure \
+		--bindir=/usr/bin \
+		--libdir=/usr/lib \
+		--runstatedir=/run \
+		--sbindir=/usr/sbin \
+		--disable-chfn-chsh \
+		--disable-login \
+		--disable-nologin \
+		--disable-su \
+		--disable-setpriv \
+		--disable-runuser \
+		--disable-pylibmount \
+		--disable-liblastlog2 \
+		--disable-static \
+		--without-python \
+		ADJTIME_PATH=/var/lib/hwclock/adjtime \
+		--docdir=/usr/share/doc/util-linux-${UTIL_LINUX_VER}
 
 	msg "Building util-linux..."
 
