4a492e6b722420507ce2da2510e994d54936570a
John Unland <unlandj2012@gmail.com>
2026-02-15 11:03:30 -0600

    Add texinfo installation steps in chroot environment

--- scripts/steps/texinfo_step.sh
+++ scripts/steps/texinfo_step.sh
@@ -22,3 +22,44 @@ step_chroot_texinfo() {
 
 	clean_work_dir
 }
+
+step_chroot_texinfo_stage3() {
+	extract_file "${SOURCES}/texinfo-${TEXINFO_VER}.tar.xz" "${WORK}/texinfo-${TEXINFO_VER}"
+
+	cd "${WORK}/texinfo-${TEXINFO_VER}"
+
+	msg "Configuring texinfo..."
+
+	sed 's/! $output_file eq/$output_file ne/' -i tp/Texinfo/Convert/*.pm
+
+	./configure --prefix=/usr
+
+	msg "Building texinfo..."
+
+	make
+
+	msg "Checking texinfo..."
+
+	make check
+
+	msg "Installing texinfo..."
+
+	make install
+
+	msg "Optional installation for texinfo..."
+
+	make TEXMF=/usr/share/texmf install-tex
+
+	pushd /usr/share/info
+	
+	rm -v dir
+	
+	for f in *
+	    msg "Recreate info docs: $f"
+	    do install-info $f dir 2>/dev/null
+	done
+	
+	popd
+
+	clean_work_dir
+}
